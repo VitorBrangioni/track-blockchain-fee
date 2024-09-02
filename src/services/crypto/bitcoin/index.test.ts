85be155f2b1a92827aed19c7e8193c974cc68b59
Vitor Brangioni <vitorh.brangioni@gmal.com>
2024-09-02 22:01:59 +0100

    test: :white_check_mark: mapped what should be tested in this application

---
+++ src/services/crypto/bitcoin/index.test.ts
@@ -0,0 +1,43 @@
+
+describe('bitcoin', () => {
+
+    describe('fetchFeeRateInSatoshi', () => {
+
+        it('should send a request a GET and return the total total value in Satoshi', () => {
+            expect(true).toBe(true);
+        });
+
+        it('should throw an error if it doesnt return a number', () => {
+            expect(true).toBe(true);
+        });
+
+        it('should throw an error if the api fails', () => {
+            expect(true).toBe(true);
+        });
+    });
+
+    describe('parseSatoshiToBTC', () => {
+
+        it('should parse satoshi to BTC correctly', () => {
+            expect(true).toBe(true);
+        });
+    });
+
+    describe('calculateSatPerVbyte', () => {
+
+        it('should calculate Satoshi per Vbyte correctly', () => {
+            expect(true).toBe(true);
+        });
+    });
+
+    describe('calculateFee', () => {
+
+        it('should calculate the estimated fee correctly', () => {
+            expect(true).toBe(true);
+        });
+
+        it('should log an error if there`s any failure.', () => {
+            expect(true).toBe(true);
+        });
+    });
+});
---
+++ src/services/crypto/ethereum/index.test.ts
@@ -0,0 +1,27 @@
+
+describe('ethereum', () => {
+
+    describe('fetchGasPrice', () => {
+
+        it('should send a request POST to call "eth_gasPrice" and return the result in Number', () => {
+            expect(true).toBe(true);
+        });
+
+        it('should throw an error if it doesnt return a number', () => {
+            expect(true).toBe(true);
+        });
+
+        it('should throw an error if the api fails', () => {
+            expect(true).toBe(true);
+        });
+    });
+
+
+    describe('calculateFee', () => {
+
+        it('should calculate correctly the estimated fee', () => {
+            expect(true).toBe(true);
+        });
+    });
+
+});
